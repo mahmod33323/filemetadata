a6dad2da1819eb7d986158d22df41fa5bd3e8abd
mahmoud <mahmod33323@gmail.com>
2021-11-07 20:50:15 +0100

    c

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+
+app.use(cors());
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
+
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+
+  res.json({
+    name : req.file.originalname,
+    type : req.file.mimetype,
+    size : req.file.size})
+
+})
+
